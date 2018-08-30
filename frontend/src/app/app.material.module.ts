43770aede74e9259aa6e052b3b9810453390d2a0
efduartem <efduartem@gmail.com>
2018-08-30 11:03:04 -0400

    Frontend Project - Init commit

---
+++ frontend/src/app/app.material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule, MatToolbarModule} from '@angular/material';
+
+@NgModule({
+    imports: [
+        MatToolbarModule, MatIconModule
+    ],
+    exports: [
+        MatToolbarModule, MatIconModule
+    ],
+})
+export class AppMaterialModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app.material.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
